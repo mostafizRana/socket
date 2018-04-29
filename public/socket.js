8c8eb17d47182af25eb8d2991856b56f2eb1ef11
mostafizRana <36274048+mostafizRana@users.noreply.github.com>
2018-04-29 23:32:27 +0600

    Update socket.js

--- public/socket.js
+++ public/socket.js
@@ -3,16 +3,13 @@ console.log('connected');
 
 socket.on('ids',function(data){ 
     document.getElementById('msg').innerHTML='';
-    // document.getElementById('chat').innerHTML+="<p class='txt'> <strong> <em>"+data[1].username+ ":</em></strong> "+data[1].message+"</p>"
     
     for (var key in data[0]) {
     if (data[0].hasOwnProperty(key)) {
         if(key===socket.id){
             document.getElementById('msg').innerHTML+='<p> You joined the conversation!</p>';
-            // document.getElementById('chat').innerHTML+='<p> '+data[0][key]+' said:'+ data[1].message+'</p>';
         } else{
             document.getElementById('msg').innerHTML+='<p>'+ data[0][key]+' joined the conversation!</p>';
-            // document.getElementById('chat').innerHTML+='<p> '+data[2]+' said:'+ data[1].message+'</p>';
         }
     }
 }
@@ -56,23 +53,3 @@ socket.on('chat',function(data){
 
 
 
-// socket.on('ids',function(data){
-//     document.getElementById('msg').innerHTML='';
-//     $("#button").unbind().click(function() {
-//     socket.emit('name',document.getElementById('username').value);
-// });
-    
-  
-                        
-        
-//     if(data.length===1){
-//         document.getElementById('msg').innerHTML='<p>waiting for other players</p>';
-//     } else {
-
-//             data.forEach(function(id){
-//       if(id!=socket.id){
-//             document.getElementById('msg').innerHTML+='<p>'+ id +' joined the game!</p>';
-//       }
-//     });
-//     }
-// });
